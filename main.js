898196c1e6dacb05fae9580b31dece54a4479af9
Officialvic <onaopeadeoye594@gmail.com>
2023-02-24 09:52:47 +0100

    mainframe high school attendance application 1.0

---
+++ main.js
@@ -0,0 +1,11 @@
+let countEl = document.getElementById("count-el");
+console.log(document.getElementById("count-el"));
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+function save() {
+
+}
